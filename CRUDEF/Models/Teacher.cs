8af0fd9ea2cecd3d22f0aca5a09bec00a72e77d0
thesaijbsj <thesaijebas007@gmail.com>
2025-01-28 11:54:41 +0530

    Add files via upload

---
+++ CRUDEF/Models/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using CRUDEF.CustomValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDEF.Models
+{
+	public class Teacher
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public string Skills { get; set; }
+
+		[Range(5, 50)]
+		public int TotalStudents { get; set; }
+
+		[Required]
+		public decimal Salary { get; set; }
+
+		[Required]
+		public DateTime AddedOn { get; set; }
+	}
+}

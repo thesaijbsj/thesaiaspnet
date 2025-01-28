f777242d707fa1f8d6ac50ec66312200108c9f45
thesaijbsj <thesaijebas007@gmail.com>
2025-01-28 11:27:04 +0530

    Add files via upload

---
+++ CRUDEF/CustomValidation/SkillsValidate.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace CRUDEF.CustomValidation
+{
+	public class SkillsValidate : Attribute, IModelValidator
+	{
+		public string[] Allowed { get; set; }
+		public string ErrorMessage { get; set; }
+		public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
+		{
+
+			if (Allowed.Contains(context.Model as string))
+				return Enumerable.Empty<ModelValidationResult>();
+			else
+				return new List<ModelValidationResult> {
+					new ModelValidationResult("", ErrorMessage)
+				};
+		}
+	}
+}
---
+++ CRUDEF/Migrations/20231004100043_Migration1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teacher",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Skills = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    TotalStudents = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<decimal>(type: "money", nullable: false),
+                    AddedOn = table.Column<DateTime>(type: "date", nullable: false, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teacher", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teacher");
+        }
+    }
+}
